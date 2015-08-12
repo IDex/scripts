dec80cfaed94c9ca18b1a4e464eeffa4a61acae0
ide94 <fuster94@gmail.com>
2015-08-12 05:51:44 +0300

    Added time delta information

--- alarm.py
+++ alarm.py
@@ -16,12 +16,13 @@ def normalAlarm():
         pass
     if dt.datetime.now() > atime:
         atime += dt.timedelta(days=1)
-    return atime
+    return (atime, atime-dt.datetime.now())
 
 
 def timerAlarm(x, mult):
     args = sys.argv[1].rstrip(x)
-    return dt.datetime.now() + dt.timedelta(minutes=int(args)*mult)
+    diff = dt.timedelta(minutes=int(args)*mult)
+    return (dt.datetime.now() + diff, diff)
 
 try:
     args = [int(a) for a in sys.argv[1].split(':')]
@@ -29,18 +30,18 @@ except:
     if sys.argv[1] == 'h':
         print(
             """
-
-ex.
-    alarm 10:00 8
-        -> alarm at 02:00
-    alarm 01:00
-        -> alarm at 01:00
-    alarm 5m
-        -> alarm 5min from now on
-    alarm 5m r
-        -> repeat the alarm
-""")
+    ex.
+        alarm 10:00 8
+            -> alarm at 02:00
+        alarm 01:00
+            -> alarm at 01:00
+        alarm 5m
+            -> alarm 5min from now on
+        alarm 5m r
+            -> repeat the alarm
+    """)
         raise SystemExit
+
 once = True
 try:
     if sys.argv[2] == 'r':
@@ -48,17 +49,20 @@ try:
 except:
     pass
 if 'm' in sys.argv[1]:
-    atime = timerAlarm('m', 1)
+    atime, dtime = timerAlarm('m', 1)
 elif 'h' in sys.argv[1]:
-    atime = timerAlarm('h', 60)
+    atime, dtime = timerAlarm('h', 60)
 else:
-    atime = normalAlarm()
+    atime, dtime = normalAlarm()
     once = False
+
 print("""\
 Given time: {}
 Alarm time: {}
+Delta: {}
 Repeat?: {}\
-""".format(sys.argv[1], atime, not once))
+""".format(sys.argv[1], atime, str(dtime).rsplit('.')[0], not once))
+
 while True:
     diff = dt.datetime.now() > atime
     if diff:
