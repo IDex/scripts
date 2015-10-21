513af54934c34926853655076afdf1a41d8be732
ide94 <fuster94@gmail.com>
2015-10-21 13:59:08 +0300

    Changed to argparse for argument parsing

--- alarm.py
+++ alarm.py
@@ -3,57 +3,51 @@ import datetime as dt
 import subprocess as sp
 import time as t
 import os
+import argparse
+
+p = argparse.ArgumentParser(
+    description='Ring alarm after set delta or at set time')
+# ex.
+#    alarm 10:00 8
+#        -> alarm at 02:00, repeat
+#    alarm 1
+#        -> alarm at 01:00, repeat
+#    alarm 5m
+#        -> alarm 5min from now on, don't repeat
+#    alarm 5m -r
+#        -> repeat the alarm
+#''')
+p.add_argument('time', metavar='T',
+               help='Alarm time, alarm: hh:mm, h; timer: xm, xh')
+p.add_argument('diff', nargs='?', default=0, metavar='D', type=int,
+               help='How many hours earlier should the alarm ring')
+p.add_argument('-r', action='store_true', default=False,
+               help='''Toggle repeat, default depends on type of time''')
+args = p.parse_args()
 
 
-def normalAlarm():
-    time = dt.time(*args)
+def normalAlarm(time, diff):
+    formattedtime = dt.time(*(int(x) for x in time.split(':')))
     today = dt.datetime.today()
-    atime = dt.datetime.combine(today, time)
-    try:
-        diff = sys.argv[2]
-        atime = atime - dt.timedelta(days=0, hours=int(diff))
-    except:
-        pass
+    atime = dt.datetime.combine(today, formattedtime)
+    atime = atime - dt.timedelta(days=0, hours=diff)
     if dt.datetime.now() > atime:
         atime += dt.timedelta(days=1)
-    return (atime, atime-dt.datetime.now())
+    return (atime, atime - dt.datetime.now())
 
 
-def timerAlarm(x, mult):
-    args = sys.argv[1].rstrip(x)
-    diff = dt.timedelta(minutes=int(args)*mult)
+def timerAlarm(time, x, mult):
+    cleantime = time.rstrip(x)
+    diff = dt.timedelta(minutes=int(cleantime) * mult)
     return (dt.datetime.now() + diff, diff)
 
-try:
-    args = [int(a) for a in sys.argv[1].split(':')]
-except:
-    if sys.argv[1] == 'h':
-        print(
-            """
-    ex.
-        alarm 10:00 8
-            -> alarm at 02:00
-        alarm 01:00
-            -> alarm at 01:00
-        alarm 5m
-            -> alarm 5min from now on
-        alarm 5m r
-            -> repeat the alarm
-    """)
-        raise SystemExit
-
-once = True
-try:
-    if 'r' in sys.argv:
-        once = False
-except:
-    pass
-if 'm' in sys.argv[1]:
-    atime, dtime = timerAlarm('m', 1)
-elif 'h' in sys.argv[1]:
-    atime, dtime = timerAlarm('h', 60)
+once = not args.r
+if 'm' in args.time:
+    atime, dtime = timerAlarm(args.time, 'm', 1)
+elif 'h' in args.time:
+    atime, dtime = timerAlarm(args.time, 'h', 60)
 else:
-    atime, dtime = normalAlarm()
+    atime, dtime = normalAlarm(args.time, args.diff)
     once = not once
 
 print("""\
@@ -61,7 +55,8 @@ Given time: {}
 Alarm time: {}
 Repeat?: {}
 Original Delta: {}\
-""".format(sys.argv[1], str(atime).rsplit('.')[0], not once, str(dtime).rsplit('.')[0]))
+""".format(sys.argv[1], str(atime).rsplit('.')[0], not once,
+           str(dtime).rsplit('.')[0]))
 
 try:
     while True:
@@ -75,7 +70,7 @@ try:
             else:
                 continue
         print('Delta: {}'.format(
-            str(atime-dt.datetime.now())).rsplit('.')[0], end='\r')
+            str(atime - dt.datetime.now())).rsplit('.')[0], end='\r')
         t.sleep(5)
 except KeyboardInterrupt:
     pass
