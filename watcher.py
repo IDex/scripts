880bcf9cf9472b9feee908629304728af340f9e7
ide94 <fuster94@gmail.com>
2015-11-02 01:59:13 +0200

    autopepped, added clearing

--- watcher.py
+++ watcher.py
@@ -31,11 +31,23 @@ class Watcher:
             print('{} while loading json'.format(e))
         return watched
 
-    def find(self, files):
+    def find(self, files, from_watched=False):
         arg = '.*'.join(files)
-        matches = [x for x in os.listdir(FOLDER)
-                   if re.search(arg, x, re.IGNORECASE) and x not in self.watched]
+        if from_watched:
+            matches = [x for x in os.listdir(FOLDER)
+                       if re.search(arg, x, re.IGNORECASE)]
+        else:
+            matches = [x for x in os.listdir(FOLDER)
+                       if re.search(arg, x, re.IGNORECASE) and x not in self.watched]
         return matches
+
+    def clear(self, regex):
+        if regex:
+            m = self.find(regex, from_watched=True)
+            self.watched = [x for x in self.watched if not x in m]
+        else:
+            self.watched.pop()
+        self.save()
 
     def playone(self, f=None):
         if not f:
@@ -67,16 +79,20 @@ class Watcher:
             return False
 
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Watch unseen videos in a folder automatically')
-    parser.add_argument('-d', '--directory', default=FOLDER, help='Specify directory for videos')
+    parser.add_argument('-d', '--directory', default=FOLDER,
+                        help='Specify directory for videos')
     parser.add_argument('-a', '--ask', action='count', default=0,
                         help='Watch one file at a time and get asked to continue')
+    parser.add_argument('-c', '--clear', action='count',
+                        help='Clear last seen/regex from watched.json')
     parser.add_argument('searchwords', nargs='*')
     args = parser.parse_args()
     watcher = Watcher(args.searchwords, args.directory)
+    if args.clear:
+        watcher.clear(args.searchwords)
     if args.ask:
         watcher.playall(ask)
     else:
